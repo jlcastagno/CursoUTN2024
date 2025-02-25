790608bd28f4622acd11faa20b4c46313eefe82c
José Luis <joseluisbrown000@gmail.com>
2025-02-25 10:38:43 -0300

    M3Utres

---
+++ M3Utres/routes/contacto.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+//  res.send('Hola soy la página de Contacto'); //
+    res.render('contacto')
+});
+
+
+
+module.exports = router;
