052512d662cedaf6d9f3250b4b026f07c9bdf0ce
jlcastagno <155312542+jlcastagno@users.noreply.github.com>
2025-03-04 13:22:02 -0300

    Update bd.js

--- M4U3/models/bd.js
+++ M4U3/models/bd.js
@@ -10,6 +10,6 @@ var pool = mysql.createPool({
     database: process.env.MYSQL_DB_NAME
 });
 
-pool.query = util.promisify(pool, query);
+pool.query = util.promisify(pool.query);
 
 module.exports = pool;
